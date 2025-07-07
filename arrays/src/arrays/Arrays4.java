e6a625242b60bc214754ce3f2ddff8a278ca8aac
Trisha <trishanadella6@gmail.com>
2025-07-07 10:08:19 +0530

    Javafundamentals

---
+++ arrays/src/arrays/Arrays4.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Arrays4 {
+
+	public static void main(String[] args) {
+		int[] asciiValues = {21, 32, 43, 54, 65}; 
+
+        System.out.println("Characters: ");
+        for (int value : asciiValues) {
+            System.out.print((char) value);
+        }
+
+	}
+
+}
