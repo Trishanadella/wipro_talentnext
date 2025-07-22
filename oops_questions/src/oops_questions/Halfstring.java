28e063b482b9eda7d2b16f9a6810ba1efb24c399
Trisha <trishanadella6@gmail.com>
2025-07-22 11:22:36 +0530

    Oops_question

---
+++ oops_questions/src/oops_questions/Halfstring.java
@@ -0,0 +1,28 @@
+package oops_questions;
+
+
+import java.util.*;
+
+public class Halfstring {
+    public static String getFirstHalf(String str) {
+        if (str.length() % 2 == 0) {
+            return str.substring(0, str.length() / 2);
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        String input1 = "TomCat";
+        String result1 = getFirstHalf(input1);
+        System.out.println("Input: " + input1);
+        System.out.println("Output: " + result1);
+
+        
+        String input2 = "Apron";
+        String result2 = getFirstHalf(input2);
+        System.out.println("Input: " + input2);
+        System.out.println("Output: " + result2);
+    }
+}
